024881c850e4628112cc872cc0329cc179560564
midlajc <mail.midlajc@gmail.com>
2022-04-17 20:47:22 +0530

    i3 status bar time and date Update

--- script.sh
+++ script.sh
@@ -2,3 +2,8 @@
 
 mv ~/.config/i3/config ~/.config/i3/.config
 ln -s $PWD/.config/i3/config ~/.config/i3/
+
+mkdir ~/.config/i3status/
+touch ~/.config/i3status/config
+mv ~/.config/i3status/config ~/.config/i3status/.config
+ln -s $PWD/.config/i3status/config ~/.config/i3status/
